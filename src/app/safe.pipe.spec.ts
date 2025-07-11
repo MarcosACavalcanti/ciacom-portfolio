5eb9d88fadfc08b10c45260ea4c2659ed404e5d3
Marcos Cavalcanti <marcosacavalcanti@hotmail.com>
2025-07-11 10:49:40 +0200

    Clean some files from the project and start to create some tests

--- src/app/safe.pipe.spec.ts
+++ src/app/safe.pipe.spec.ts
@@ -1,11 +1,33 @@
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl, SafeValue } from '@angular/platform-browser';
 import { SafePipe } from './safe.pipe';
-import { Sanitizer } from '@angular/core';
+import { Sanitizer, SecurityContext } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+class MockSanitizer implements DomSanitizer {
+  sanitize(context: SecurityContext, value: SafeValue | string | null): string | null {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustStyle(value: string): SafeStyle {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustScript(value: string): SafeScript {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustUrl(value: string): SafeUrl {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustResourceUrl(value: string): SafeResourceUrl {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustHtml(value: string){
+    return value as SafeHtml;
+  }
+}
 
 describe('SafePipe', () => {
   it('create an instance', () => {
-    /// Implement test for SafePipe
-    const pipe = new SafePipe();
-    expect(pipe).toBeTruthy();
+    const pipe = new SafePipe(new MockSanitizer());
+    const result = pipe.transform('Test');
+    expect(result).toEqual('SafeHtml');
   });
 });

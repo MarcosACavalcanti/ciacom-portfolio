56cdd682620bf34db965aac2cdb3d6cad34f5b31
Marcos Cavalcanti <marcosacavalcanti@hotmail.com>
2025-07-31 12:11:17 +0200

    make some improvements in flag buttons and move all the skils and experience to a list inside the component

--- src/app/pages/page-about-me/page-about-me.component.ts
+++ src/app/pages/page-about-me/page-about-me.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-about-me.component.css']
 })
 export class PageAboutMeComponent {
+  techSkills: string[] = [
+    'NodeJS',
+    'Angular',
+    'Express',
+    'NestJS',
+    'NX',
+    'MongoDB',
+  ];
 
+  experiences: string[] = [
+    'Software Engineer | PureSpectrum |  2020 ~',
+    'Analista de Sistemas Pleno | BRQ Digital Solutions |  2018 - 2020',
+    'Intern | Korp Sistemas de gestão |  2018',
+  ];
 }
